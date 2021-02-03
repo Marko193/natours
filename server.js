cf22e52f3735f3114222b64b5b376092f12c03d2
Marko Pavlenko <marko.pavlenko@nure.ua>
2021-02-03 20:23:48 +0200

    Create a Tour Schema and a Model by its

--- server.js
+++ server.js
@@ -19,6 +19,28 @@ mongoose
         console.log('DB Connection successfully!');
     })
 
+//A simple Tour Schema - desc our data
+//we have a name & a datatype
+//specify the schema and doing the validation
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'], //a validator
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price!']
+    }
+});
+
+//Create a Model by this Schema
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);

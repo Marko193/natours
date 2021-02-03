f9a45984c14b8e88977538122ed6bc0a84e0e229
Marko Pavlenko <marko.pavlenko@nure.ua>
2021-02-03 19:52:30 +0200

    Connected to GLOBAL (and LOCAL) MongoDB

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+//We should define in a config file _LOCAL and _GLOBAL var for connection
+//    .connect(process.env.DATABASE_LOCAL, {  //connection to the local DB
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }).then(() => {
+        //console.log(con.connections);
+        console.log('DB Connection successfully!');
+    })
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

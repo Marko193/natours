35314ed704e557cde0d59a5ad04b95b9b9e784e3
Marko Pavlenko <marko.pavlenko@nure.ua>
2021-02-08 17:17:53 +0200

    Development & Production Errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,44 @@
 //`Can't find ${ req.originalUrl } on this server!`
-
-module.exports = (err, req, res, next) => {
-
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error'
+const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
-        message: err.message
+        error: err,
+        message: err.message,
+        stack: err.stack,
     });
-}
+};
+
+const sendErrorProduction = (err, res) => {
+    //Operational, trusted error: send message to client
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+        //Programing or other unknown error: don`t leak details to the client
+    } else {
+        //1) Log error
+        console.error('ERROR!');
+
+        //2) Send generic message
+
+
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong!'
+        });
+    }
+};
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    //if we`re on the development stage
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+        //if we`re on the production stage
+    } else if (process.env.NODE_ENV === 'production') {
+        sendErrorProduction(err, res)
+    }
+};
--- utils/appError.js
+++ utils/appError.js
@@ -1,10 +1,12 @@
 class AppError extends Error {
     constructor(message, statusCode) {
         super(message); //to call the parent constructor
+
         this.statusCode = statusCode;
         this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-        //all err with this property = true 
+        //ALL ERR THAT WE`RE CREATED BY OURSELVES - OPERATIONAL ERR
         this.isOperational = true;
+
         Error.captureStackTrace(this, this.constructor);
     }
 }

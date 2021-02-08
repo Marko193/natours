463fb2a6b6d4615e50d5357e2add94fee2f99a27
Marko Pavlenko <marko.pavlenko@nure.ua>
2021-02-08 13:32:58 +0200

    Imolementing a global error handling middleware

--- app.js
+++ app.js
@@ -44,12 +44,29 @@ app.use('/api/v1/users', userRouter);
 
 
 //for all others URL`s, which didn`t handle before
-//after all of the routes
+//after all of the routes!!!
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'Fail!',
-        message: `Can't find ${ req.originalUrl } on this server!`
-    })
+    // res.status(404).json({
+    //     status: 'Fail!',
+    //     message: `Can't find ${ req.originalUrl } on this server!`
+    // });
+
+    //Init error
+    const err = new Error(`Can't find ${ req.originalUrl } on this server!`);
+    err.status = "fail";
+    err.statusCode = 404;
+
+    next(err);
+});
+
+//A GLOBAL ERROR HANDLING MIDDLEWARE
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error'
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
 });
 
 module.exports = app;

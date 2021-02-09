bde89b678be3dac898987f86363798a33e5ef650
Marko Pavlenko <marko.pavlenko@nure.ua>
2021-02-09 21:47:32 +0200

    Password encryption (hashing) by bcryptjs

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 //const slugify = require('slugify');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -24,12 +25,32 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Please, create a password!'],
-        minlength: 8
+        minlength: 5
     },
     passwordConfirm: {
         type: String,
-        required: [true, 'Please confirm yout password!']
+        required: [true, 'Please confirm yout password!'],
+        validate: {
+            //This only works on CREATE && SAVE!!!
+            validator: function(el) {
+                return el === this.password; //abc === abc
+            },
+            message: 'Passwords are not the same!'
+        }
     }
+});
+
+//Passwor encryptor
+userSchema.pre('save', async function(next) {
+    //Only run this f() if password was modified
+    if (!this.isModified('password')) return next();
+
+    //Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    //Delete the passwordConfirm field
+    this.passwordConfirm = undefined;
+    next();
 });
 
 const User = mongoose.model('User', userSchema);

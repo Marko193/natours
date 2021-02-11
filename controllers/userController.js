6e2cb6705c6ac1f6f37a553761769cf24af3842b
Marko Pavlenko <marko.pavlenko@nure.ua>
2021-02-12 01:14:14 +0200

    Implem the updating of defined current user`s data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,21 @@
 //const fs = require('fs');
 //const APIFeatures = require('./../utils/apiFeatures');
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
 //USERS ROUTES
+
+const filterObj = (obj, ...allowedFields) => {
+    //loop throw in obj
+    const newObj = {};
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) {
+            newObj[el] = obj[el];
+        }
+    });
+    return newObj;
+};
 
 exports.getAllUsers = catchAsync(async(req, res, next) => {
     const users = await User.find();
@@ -16,6 +28,31 @@ exports.getAllUsers = catchAsync(async(req, res, next) => {
             users: users
         }
     });
+});
+
+//update the current user data
+exports.updateMe = catchAsync(async(req, res, next) => {
+    //1. Create err if user POSTs password data
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(new AppError('This route isn`t for password updates! Please use /updateMyPassword', 400));
+    }
+
+    //2. Filtered out unwanted fields names that are not allowed to be updated
+    //body.role: 'admin' - don`t let the user to change his status
+    const filteredBody = filterObj(req.body, 'name', 'email');
+
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser
+        }
+
+    })
 });
 
 exports.createUser = (req, res) => {

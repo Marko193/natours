20771660863d33da7ebf69215dcd4bc0a780b6d5
Marko Pavlenko <marko.pavlenko@nure.ua>
2021-02-16 20:05:54 +0200

    Implemented The Complex Import Mechanism

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -19,11 +21,15 @@ mongoose
 
 //READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'));
 
 //IMPORT DATA INTO COLLECTION
 const importData = async() => {
     try {
         await Tour.create(tours);
+        await User.create(users, { validateBeforeSave: false });
+        await Review.create(reviews);
         console.log('Data is successfully loaded!');
         process.exit();
     } catch (err) {
@@ -34,7 +40,9 @@ const importData = async() => {
 //DELETE ALL DATA FROM COLLECTION
 const deleteData = async() => {
     try {
-        await Tour.deleteMany(); //del all docs in the collection
+        await Tour.deleteMany();
+        await User.deleteMany();
+        await Review.deleteMany();
         console.log('Data is successfully deleted!');
         process.exit();
     } catch (err) {
